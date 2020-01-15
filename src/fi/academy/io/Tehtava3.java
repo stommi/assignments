de449ec845620803cdf22b6a954de43d00126bad
TommiS <tommi.supi@gmail.com>
2020-01-15 15:08:31 +0200

    lisätty luokka tehtava3

--- src/fi/academy/io/Tehtava3.java
+++ src/fi/academy/io/Tehtava3.java
@@ -1,4 +1,31 @@
 package fi.academy.io;
+import java.util.Scanner;
+import java.io.File;
 
 public class Tehtava3 {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Mitä tiedostoa tutkitaan");
+        String tiedosto = lukija.nextLine();
+        int rivienMaara = 0;
+        int merkkienMaara = 0;
+
+        try (Scanner tiedostonLukija = new Scanner(new File(tiedosto))) {
+
+            while (tiedostonLukija.hasNextLine()) {
+                String rivi = tiedostonLukija.nextLine();
+                System.out.println(rivi);
+                rivienMaara++;
+                merkkienMaara+=rivi.length();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Virhe: " + e.getMessage());
+        }
+
+        System.out.println("Tiedostossa on " + rivienMaara + " riviä");
+        System.out.println("Tiedostossa on " + +merkkienMaara + " merkkiä");
+    }
+
 }
