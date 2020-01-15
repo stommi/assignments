e6b12628df54a7fbab13df7a2cab3ee9021e69c5
TommiS <tommi.supi@gmail.com>
2020-01-15 13:21:47 +0200

    lisätty luokka tehtava2

---
+++ src/fi/academy/regex/Harjoitus2.java
@@ -0,0 +1,26 @@
+package fi.academy.regex;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Harjoitus2 {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        List<String> tulokset = new ArrayList<String>();
+
+        String syote = lukija.nextLine();
+        Pattern pat = Pattern.compile("Academy");
+        Matcher m = pat.matcher(syote);
+
+        for (; m.find() ;) {
+            tulokset.add(m.start() + ": " + m.group());
+        }
+        tulokset.forEach(System.out::println);
+
+    }
+}
